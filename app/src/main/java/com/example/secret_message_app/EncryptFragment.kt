72577e005c3398bd75c7706ba1eeff4a9262cebc
Shadab <shadabjhu00447@gmail.com>
2023-09-20 18:17:46 +0530

    SecretmeaageApp

---
+++ app/src/main/java/com/example/secret_message_app/EncryptFragment.kt
@@ -0,0 +1,26 @@
+package com.example.secret_message_app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class EncryptFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val encryptedView = view.findViewById<TextView>(R.id.encrypted_message)
+         encryptedView.text = message.reversed()
+        return view
+    }
+
+
+}
